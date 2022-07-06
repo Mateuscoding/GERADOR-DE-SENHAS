5c986392e67d5e47b3eaf3f4dfeaa74788e3dc68
Mateus Bezerra <mateusbezerra7k@gmail.com>
2022-07-06 12:19:19 -0300

    Refactoring
    Design/Estrutura/

--- js/script.js
+++ js/script.js
@@ -1,52 +1,54 @@
 
-let btnGerar = document.querySelector('#gerar')
-let senhaGerada = document.querySelector('#senha')
-let divSenha = document.querySelector('#divSenha')
-let btnCopiar = document.querySelector('#copiar')
-let letras = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+let btnGenerate = document.querySelector('#generate')
+let newPassoword = document.querySelector('#password')
+let responseContainer = document.querySelector('#response-container')
+let btnCopy = document.querySelector('#copy')
 
-let numeros = '0123456789'
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz' + '0123456789'
 
+function generatePassword(length){ // Função limitada a quantidade de caracteres (length) para gerar uma letra ou número aleatório da variável "characters"
 
+    return length == 0?' ': 
+        characters.charAt(Math.floor(Math.random() * characters.length)) + generatePassword(length -1)
+}
 
-btnGerar.addEventListener('click', ()=>{
-
-    let digitos = parseInt(document.querySelector('#tamanho-senha').value)
-    // console.log(digitos)
-
-    let senha = ''
-
-    for(let i = 0; i <= digitos; i++){
-
-        let letrasOuNumeros = Math.floor(Math.random()*2)
-
-        if(letrasOuNumeros > 0){
-            
-            let letra = Math.floor(Math.random()*52)
-
-            senha += letras[letra]
-            
-            // console.log(letras[letra])
-
-        }else{
-
-            let numero = Math.floor(Math.random()*10)
-            senha+= numeros[numero]
-
-            console.log(numeros[numero])
-
-        }
-
-    }
-        
-    divSenha.classList.remove('of')
-    senhaGerada.textContent = senha
-
+btnGenerate.addEventListener('click', ()=>{
+    
+    let length = parseInt(document.querySelector('#length-password').
+    value)
+    
+    responseContainer.classList.remove('of')
+    newPassoword.textContent = generatePassword(length)
+       
 })
 
-
-btnCopiar.addEventListener('click', ()=>{
+btnCopy.addEventListener('click', ()=>{ // Função do botão para copiar a senha (Ctrl c + Ctrl v )
         alert('Senha copiada com sucesso!')
-        navigator.clipboard.writeText(senhaGerada.textContent)
+        navigator.clipboard.writeText(newPassoword.textContent)
     }
 )
+
+
+
+
+
+// btnGenerate.addEventListener('click', ()=>{
+
+//     let digitos = parseInt(document.querySelector('#tamanho-senha').value)
+
+//     let senha = ''
+
+//     for(let i = 0; i <= digitos; i++){
+
+//         let letrasOuNumeros = Math.floor(Math.random()*2)
+//         if(letrasOuNumeros > 0){
+//             let letra = Math.floor(Math.random()*52)
+//             senha += letras[letra]
+//         }else{
+//             let numero = Math.floor(Math.random()*10)
+//             senha+= numeros[numero]
+//         }
+//     }  
+//     responseContainer.classList.remove('of')
+//     newPassoword.textContent = senha
+// })
